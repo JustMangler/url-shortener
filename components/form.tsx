f33d44a6cf4732037ccb083252ae0fe5d5d87eab
William Zhou <william.zhou@duke.edu>
2023-12-01 00:37:40 -0500

    finishing form

--- components/form.tsx
+++ components/form.tsx
@@ -1,38 +1,57 @@
+import { FaLock } from "react-icons/fa";
+
 export default function Form() {
   return (
     <div>
-      <div className="flex max-w-3xl  bg-white shadow-md rounded-3xl mx-auto">
+      <div className="flex max-w-4xl bg-white shadow-md rounded-3xl mx-auto">
         <form className="w-full p-8">
-          <div className="flex flex-col md:items-left mb-6">
+          <div className="flex flex-col md:items-left mb-6 ">
             <h1 className="text-left font-bold text-3xl pb-3">
               Shorten a long URL
             </h1>
-            <label className="block text-lg font-bold text-left pb-2">
-              Paste a long link
-            </label>
-            <input
-              className="appearance-none border-2 border-gray-400 rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:bg-white focus:border-purple-500"
-              id="inline-full-name"
-              type="text"
-              placeholder="Example: https://ezurl.link/super-long-link"
-            />
-            <label className="block text-lg font-bold text-left pt-6 pb-2">
-              Customize your link{" "}
-              <span className="font-normal">{"(optional)"}</span>
-            </label>
-            <div className="flex ">
-              <div
-                className="text-left appearance-none border-2 border-gray-400 rounded w-1/3 py-2 px-3 text-gray-700 focus:outline-none focus:bg-white focus:border-purple-500"
-                id="inline-full-name"
-              >
-                ezurl.link
+            <div className="grid grid-cols-1 md:grid-cols-5">
+              <div className="col-span-1 md:col-span-5 ml-2 mb-2 mr-2">
+                <label className="block text-lg font-bold text-left pb-2">
+                  Paste a long link
+                </label>
+                <input
+                  className="appearance-none border-2 border-gray-400 rounded-lg w-full py-2 px-3 text-gray-700 focus:outline-none focus:bg-white focus:border-sky-600"
+                  id="inline-full-name"
+                  type="text"
+                  placeholder="Example: https://ezurl.link/super-long-link"
+                />
               </div>
-              <input
-                className="appearance-none border-2 border-gray-400 rounded w-2/3 py-2 px-3 text-gray-700 focus:outline-none focus:bg-white focus:border-purple-500"
-                id="inline-full-name"
-                type="text"
-                placeholder="Example: https://ezurl.link/super-long-link"
-              />
+              <div className="col-span-1 md:col-span-2 pt-2 ml-2 mb-2">
+                <label className="block text-lg font-bold text-left  pb-2">
+                  Domain
+                </label>
+
+                <div className="flex flex-row ">
+                  <div
+                    className="flex flex-row text-left items-center justify-between appearance-none border-2 border-gray-400 w-full rounded-lg py-2 px-3 text-gray-400 focus:outline-none bg-gray-100 focus:border-sky-600"
+                    id="inline-full-name"
+                  >
+                    ezurl.link
+                    <FaLock />
+                  </div>
+                  <div className="flex items-center pl-2">
+                    <label>/</label>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-1 md:col-span-3 ml-2 pt-2 mb-2 mr-2">
+                <label className="block text-lg font-bold text-left pb-2">
+                  Customize your link{" "}
+                  <span className="font-normal">{"(optional)"}</span>
+                </label>
+
+                <input
+                  className="appearance-none border-2 border-gray-400 rounded-lg w-full py-2 px-3 text-gray-700 focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="inline-full-name"
+                  type="text"
+                  placeholder="example: make-money"
+                />
+              </div>
             </div>
           </div>
 
