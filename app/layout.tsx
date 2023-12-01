d7fed5c3ed29e4426299a77036e6b8a4bd7eff2a
William Zhou <william.zhou@duke.edu>
2023-12-01 17:05:46 -0500

    adding ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "URL Shortener - Short Links & Analytics, Get Paid | EZurl",
@@ -14,6 +15,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        id="Adsense-id"
+        data-ad-client="ca-pub-9679570807975669"
+        async
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
       <body className="bg-white">{children}</body>
     </html>
   );
